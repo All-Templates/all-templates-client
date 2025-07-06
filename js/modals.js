cb44507572cf54420f8da26c24ad9d220b133e89
TishkaGit <yatelo@vk.com>
2025-07-06 20:36:21 +0300

    Update modals.js

--- js/modals.js
+++ js/modals.js
@@ -73,14 +73,3 @@
                 form.reset();
             });
         });
-        
-        // Обработка кликов по сердечкам
-        document.querySelectorAll('.favorite-btn').forEach(btn => {
-            btn.addEventListener('click', function(e) {
-                e.stopPropagation();
-                this.classList.toggle('active');
-                this.innerHTML = this.classList.contains('active') 
-                    ? '<i class="fas fa-heart"></i>' 
-                    : '<i class="far fa-heart"></i>';
-            });
-        });
