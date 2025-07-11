3e383fc8d9a73066ee13239fbf5d53497418548e
TishkaGit <yatelo@vk.com>
2025-07-11 14:24:31 +0300

    Create api-config.js

---
+++ api-config.js
@@ -0,0 +1,10 @@
+const API_CONFIG = {
+    BASE_URL: 'https://finely-mature-naiad.cloudpub.ru',
+    ENDPOINTS: {
+        TEMPLATES: '/api/templates',
+        DOWNLOAD: '/api/templates/download',
+        CREATE: '/api/templates/create',
+        TEMPLATE_DETAILS: '/api/templates'
+    }
+};
+export default API_CONFIG;
